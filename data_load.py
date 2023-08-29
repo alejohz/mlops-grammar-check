b895826b0039565932c207ea5c6682d6a03b9b4d
Alejandro Henao <alejandrohenao91@gmail.com>
2023-08-29 18:07:31 -0500

    📝 Add documentation for functions and comments for clarity

--- data_load.py
+++ data_load.py
@@ -3,6 +3,8 @@ from datasets import load_dataset
 from torch.utils.data import DataLoader
 from transformers import AutoTokenizer
 
+# This project uses pytorch lighting to avoid some pytorch boilerplate
+
 
 class DataModule(pl.LightningDataModule):
     def __init__(self, model_name="google/bert_uncased_L-2_H-128_A-2", batch_size=32):
@@ -12,9 +14,10 @@ class DataModule(pl.LightningDataModule):
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
 
     def prepare_data(self):
-        cola_dataset = load_dataset("glue", "cola")
-        self.train_data = cola_dataset["train"]
-        self.val_data = cola_dataset["validation"]
+        # COLA means Corpus of Linguistic Acceptability
+        cola_dataset = load_dataset("glue", "cola")  # Load dataset
+        self.train_data = cola_dataset["train"]  # Split into train and validation
+        self.val_data = cola_dataset["validation"]  # This split comes from hf datasets
 
     def tokenize_data(self, example):
         return self.tokenizer(
@@ -26,6 +29,7 @@ class DataModule(pl.LightningDataModule):
 
     def setup(self, stage=None):
         if stage == "fit" or stage is None:
+            # Stage check to avoid loading unnecesary data
             self.train_data = self.train_data.map(self.tokenize_data, batched=True)
             self.train_data.set_format(
                 type="torch", columns=["input_ids", "attention_mask", "label"]
@@ -41,3 +45,15 @@ class DataModule(pl.LightningDataModule):
 
     def val_dataloader(self):
         return DataLoader(self.val_data, batch_size=self.batch_size, shuffle=False)
+
+
+if __name__ == "__main__":
+    # Add manual data validation
+    data_model = DataModule()
+    data_model.prepare_data()
+    data_model.setup()
+    # Print to check shape
+    # Shape is very important when using pytorch
+    print(next(iter(data_model.train_dataloader()))["input_ids"].shape)
+    # torch.Size([32, 512])
+    # This is the size expected due to model batch being 32 and max_length being 512
